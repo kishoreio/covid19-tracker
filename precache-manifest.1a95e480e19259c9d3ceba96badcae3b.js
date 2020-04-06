b834659390f9e709174d5a6cf2cf0292fd2435dd
Kishore S <30261988+kishoreio@users.noreply.github.com>
2020-04-06 15:58:12 +0530

    Updates

---
+++ precache-manifest.1a95e480e19259c9d3ceba96badcae3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b3785b4f4289e072ca35e90a06b29c",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "67fe5caa4f8f200416f7",
+    "url": "/covid19-tracker/static/css/main.9d978711.chunk.css"
+  },
+  {
+    "revision": "7b77d53b7f05c0164f2d",
+    "url": "/covid19-tracker/static/js/2.389210f7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.389210f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67fe5caa4f8f200416f7",
+    "url": "/covid19-tracker/static/js/main.9682ca39.chunk.js"
+  },
+  {
+    "revision": "efb4724b6063bf7d0ee6",
+    "url": "/covid19-tracker/static/js/runtime-main.9faf7677.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
